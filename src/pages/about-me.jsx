58ee3a01f514bb9461709055c4d9bc9536d0d674
calebmchenry <calebmchenry@gmail.com>
2020-04-14 18:39:19 -0400

    fix: update non home pages

--- src/pages/about-me.jsx
+++ src/pages/about-me.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import Layout from '../components/Layout/Layout';
 import SEO from '../components/seo';
 
-const SecondPage = () => (
+const AboutMe = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <SEO title="About me" />
+    <h1>About me</h1>
   </Layout>
 );
 
-export default SecondPage;
+export default AboutMe;
---
+++ src/pages/projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout/Layout';
+import SEO from '../components/seo';
+
+const Projects = () => (
+  <Layout>
+    <SEO title="Projects" />
+    <h1>Projects</h1>
+  </Layout>
+);
+
+export default Projects;
